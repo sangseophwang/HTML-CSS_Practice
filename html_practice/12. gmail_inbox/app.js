f143fa06a7438841b7d1ea7d4e4fc7859b59f1ec
SEOP <sshwang12345@gmail.com>
2021-05-24 17:03:06 +0900

    updated html_gmail_inbox

---
+++ html_practice/12. gmail_inbox/app.js
@@ -0,0 +1,22 @@
+const inboxCheckboxes = document.querySelectorAll(".inbox-checkbox");
+const inboxStars = document.querySelectorAll(".inbox-star");
+
+inboxCheckboxes.forEach(function (item) {
+  item.addEventListener("click", toggle);
+  item.active = false;
+});
+
+inboxStars.forEach(function (item) {
+  item.addEventListener("click", toggle);
+  item.active = false;
+});
+
+function toggle() {
+  if (this.active) {
+    this.classList.remove("active");
+  } else {
+    this.classList.add("active");
+  }
+
+  this.active = !this.active;
+}
